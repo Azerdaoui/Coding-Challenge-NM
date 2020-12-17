8ecc1d197e2c32804f85e542ae1331b523aedf46
AZERDAOUI othmane <oazerdaoui@thenextclic.com>
2020-12-17 17:20:46 +0100

    Create models & migrations

---
+++ app/Models/Category.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Product;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function children()
+    {
+        return $this->hasMany(Category::class,  'parent_id', 'id');
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
+}
